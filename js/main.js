a79651bf7697429ce3c76e1880fda8a7862a5a7e
unknown <victormendesmelo@hotmail.com>
2020-12-15 20:25:04 -0300

    Adicionando JS a page

---
+++ js/main.js
@@ -0,0 +1,37 @@
+function carregada(){
+    alert("Págica carregada com sucesso!")
+}
+function change(elemento){
+    console.log(elemento.value)
+}
+/*function trocar(){                                      //Função para dizer que passe o mouse
+    document.getElementById("mousemove").innerHTML = "Voce passou o mouse!"
+}
+function voltar(){                                      //Função  para dizer que passe novamente o mouse          
+    document.getElementById("mousemove").innerHTML = "Passe o mouse dnv"
+}*/
+
+/*function redirecionar(){                              //Função para redirecionar para algun site
+    window.open("https://www.youtube.com/");                 // Abre uma guia com a URL passada
+    window.location.href="https://www.youtube.com/";         //Muda a mesma janela para a URL passada
+} */  
+    /*function clickou(){                                 //Aparecer mensagem apos click no butao 
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clickar, agora vc esta sendo hackeado<b>";
+}*/
+////////////////////////////////////////////////////////////////////////////////////////
+/*function soma(n1, n2){                              //Soma por função
+
+    return n1 + n2;
+}
+alert(soma(5,18)); */
+////////////////////////////////////////////////////////////////////////////////////////
+/*var nome = "Victor Mendes";                        //Mostrar nome na caixa de mensagem
+alert("Bem vindo ao seu site " + nome);*/
+////////////////////////////////////////////////////////////////////////////////////////
+/*var idade = prompt("Qual sua idade ?")              //Perguntar idade por caixa de mensagem.
+if (idade >= 18){
+    alert("Maior de idade, parabéns!")
+}
+else{
+    alert("Menor de idade, sinto muito!")
+}*/ 
